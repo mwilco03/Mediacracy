9e80a71d26418118152f9b97cb409849a2dc7035
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-06-01 10:25:43 -0400

    Create vpn_update.sh

---
+++ gluetun/vpn_update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define variables
+gluetun_dir=$1 #"/home/plex/gluetun"
+docker_compose_file="${gluetun_dir}/docker-compose.yml"
+servers_json="${gluetun_dir}/servers.json"
+gluetun_volume="${gluetun_dir}:/gluetun"
+
+# Change directory to gluetun
+cd "$gluetun_dir" || exit 1
+docker run --rm -v "${gluetun_volume}" $docker_image format-servers -mullvad > /dev/null
+chown plex:plex "${servers_json}"
+# Parse and prioritize servers, and remove the trailing comma
+new_hostnames=$(jq -r '.mullvad.servers[] | select(.vpn == "openvpn" and .country == "USA") | .hostname' "$servers_json" | \
+    awk '{print $1".relays.mullvad.net"}' | \
+    ./prioritizer.sh /dev/stdin 2>/dev/null | \
+    sed 's/\.relays\.mullvad\.net//g' | head | tr "\n" "," | sed 's/,$//')
+
+# Update docker-compose.yml with the new server hostnames using sed for in-place modification
+sed -i -r "s/(SERVER_HOSTNAMES=).*/\1$new_hostnames/" "$docker_compose_file"
+
+# Change ownership of the docker-compose.yml file
+chown $USER:$USER "$docker_compose_file"
+
+# Display the updated docker-compose.yml
+cat "$docker_compose_file"
